1b33c9cbe3529a5ef6161ff0b162ca26dce20cfd
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-25 11:41:38 +0530

    Add project files.

---
+++ 24SOECE13010_window_app/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace _24SOECE13010_window_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.next_pg = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(367, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "welcom 😊🙏\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // next_pg
+            // 
+            this.next_pg.Location = new System.Drawing.Point(367, 296);
+            this.next_pg.Name = "next_pg";
+            this.next_pg.Size = new System.Drawing.Size(75, 23);
+            this.next_pg.TabIndex = 1;
+            this.next_pg.Text = "clik me";
+            this.next_pg.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.next_pg);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "My App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button next_pg;
+    }
+}
+
---
+++ 24SOECE13010_window_app/Form3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _24SOECE13010_window_app
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if()
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
